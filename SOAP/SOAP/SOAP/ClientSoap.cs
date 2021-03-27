a700d2de083b0c538df20a177ef413ddf922c1d7
Дмитрий Тетерев <diman.teterev@mail.ru>
2021-03-27 12:54:28 +0300

    SOAP

---
+++ SOAP/SOAP/SOAP/ClientSoap.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceReference1;
+
+namespace SOAP
+{
+    class ClientSoap
+    {
+        CalculatorSoap client = new CalculatorSoapClient();
+    }
+}
---
+++ SOAP/SoapFraemwork/SoapFraemwork/SoapClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SoapFraemwork
+{
+    class SoapClient
+    {
+        Calculator.CalculatorSoapClient client = new Calculator.CalculatorSoapClient();
+
+
+        public int Add(int a, int b)
+        {
+            return client.Add(a, b);
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return client.Subtract(a, b);
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return client.Multiply(a, b);
+        }
+
+        public int Divide(int a, int b)
+        {
+            if(b == 0)
+            {
+                return 0;
+            }
+            return client.Divide(a, b);
+        }
+    }
+}
