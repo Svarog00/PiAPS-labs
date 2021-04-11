a3ef6350f1bbd3bc9043f33a7e3f35c4d83fd000
Дмитрий Тетерев <diman.teterev@mail.ru>
2021-04-11 16:06:55 +0300

    6 proto

---
+++ SoapWebService/SOAPWebService/SoapWebServiceSer/Race.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoapWebServiceSer
+{
+	class Race
+	{
+		public struct SeatPlace
+		{
+			public int Num { get; set; }
+			public bool IsReserved { get; set; }
+
+			public SeatPlace(int num, bool isReserved)
+            {
+				Num = num;
+				IsReserved = isReserved;
+            }
+		}
+
+		private int _id;
+		private string _source;
+		private string _destination;
+		public SeatPlace[] mas;
+		public int TicketsAmount 
+		{
+			get;
+			private set;
+		}
+		
+		public Race()
+		{
+			_id = -1;
+			_source = "none";
+			_destination = "none";
+			//mas = new SeatPlace[24];
+			TicketsAmount = 0;
+		}
+		public Race(int id, string source, string destinition, int ticketsAmount, int maxSeat)
+		{
+			_id = id;
+			_source = source;
+			_destination = destinition;
+			TicketsAmount = ticketsAmount;
+		}
+	}
+}
